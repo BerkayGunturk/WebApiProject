4badd0fcccb1e28360030b3be6a3ee624f971756
Berkay Güntürk <78702311+BerkayGunturk@users.noreply.github.com>
2023-06-01 02:12:32 +0300

    Partials

---
+++ ApiConsume/HotelProject.WebAPİ/Controllers/SubscribeController.cs
@@ -0,0 +1,51 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebAPİ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscribeController : ControllerBase
+    {
+        private readonly ISubscribeService _subscribeSubscribe;
+
+        public SubscribeController(ISubscribeService subscribeSubscribe)
+        {
+            _subscribeSubscribe = subscribeSubscribe;
+        }
+
+        [HttpGet]
+        public IActionResult SubscribeList()
+        {
+            var values = _subscribeSubscribe.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddSubscribe(Subscribe subscribe)
+        {
+            _subscribeSubscribe.TInsert(subscribe);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteSubscribe(int id)
+        {
+            var values = _subscribeSubscribe.TGetByID(id);
+            _subscribeSubscribe.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateSubscribe(Subscribe subscribe)
+        {
+            _subscribeSubscribe.TUpdate(subscribe);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetSubscribe(int id)
+        {
+            var values = _subscribeSubscribe.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
